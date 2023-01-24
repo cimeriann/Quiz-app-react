f84889a477360794e3b2d57ebf42d05bc6255f39
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-01-24 21:32:53 +0100

    create funcs to test api endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+// Components
 import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./components/Api";
+//Types
+import { Difficulty } from "./components/Api";
 
 const TOTAL_QUESTIONS = 10;
 function App() {
@@ -13,6 +17,7 @@ function App() {
   const startTrivia = async () => {};
   const checkAnswer = async (e: React.MouseEvent<HTMLButtonElement>) => {};
   const displayNextQuestion = async () => {};
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
   return (
     <div className="App">
       <h1>React Quiz</h1>
@@ -21,14 +26,14 @@ function App() {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNumber={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={displayNextQuestion}>
         Next Question
       </button>
--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,11 +1,26 @@
+import { type } from "@testing-library/user-event/dist/type";
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
   HARD = "hard",
 }
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  console.log(data);
 };
